19d04c0353a9a8ba9af6d00d0e15db2dfc94a4fa
Andrew <andrewsafonovpost@gmail.com>
2025-06-15 21:55:23 +0300

    Added social authentication

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
 
     # 3rd party
+    'social_django',
     'widget_tweaks',
 
     # project apps
@@ -41,7 +42,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
     # 3rd party
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -134,6 +137,32 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 AUTHENTICATION_BACKENDS = [
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.github.GithubOAuth2',
     'apps.accounts.backends.UsernameOrEmailLoginBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+# Social auth config
+
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_CLIENT_ID', cast=str)
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_CLIENT_SECRET', cast=str)
+
+SOCIAL_AUTH_GITHUB_KEY = config('GITHUB_CLIENT_ID', cast=str)
+SOCIAL_AUTH_GITHUB_SECRET = config('GITHUB_CLIENT_SECRET', cast=str)
+SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'config.pipeline.verify_social_email',
+    'social_core.pipeline.user.user_details',
+)
--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('apps.accounts.urls'))
+    path('accounts/', include('apps.accounts.urls')),
+    path('accounts/social/', include('social_django.urls', namespace='social')),
 ]
