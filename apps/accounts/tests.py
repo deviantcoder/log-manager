3280f5c7f83febcb7df27f8d2138c884db2fa238
deviant <andrewsafonovpost@gmail.com>
2025-06-25 22:24:22 +0300

    Added signup tests

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -6,9 +6,10 @@ from django.conf import settings
 
 
 User = get_user_model()
+login_url = getattr(settings, 'LOGIN_URL')
 
 
-class TestAuthViews(TestCase):
+class TestUserLogin(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
             email='testuser@example.com',
@@ -16,7 +17,7 @@ class TestAuthViews(TestCase):
             password='testpass123'
         )
         self.password = 'testpass123'
-        self.login_url = reverse(getattr(settings, 'LOGIN_URL'))
+        self.login_url = reverse(login_url)
         self.logout_url = reverse('accounts:logout')
 
     def test_user_exists(self):
@@ -60,6 +61,18 @@ class TestAuthViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(get_user(self.client).is_authenticated)
 
+
+class TestUserLogout(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='testuser@example.com',
+            username='testuser',
+            password='testpass123'
+        )
+        self.password = 'testpass123'
+        self.login_url = reverse(getattr(settings, 'LOGIN_URL'))
+        self.logout_url = reverse('accounts:logout')
+
     def test_user_logout(self):
         self.client.post(
             self.login_url,
@@ -78,3 +91,45 @@ class TestAuthViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertFalse(get_user(self.client).is_authenticated)
         self.assertEqual(type(get_user(self.client)), AnonymousUser)
+
+
+class TestUserSignup(TestCase):
+    def setUp(self):
+        self.signup_url = reverse('accounts:signup')
+        self.login_url = reverse(login_url)
+        self.username = 'testuser'
+        self.first_name = 'John'
+        self.last_name = 'Doe'
+        self.email = 'testuser@example.com'
+        self.password = 'testpass123'
+
+    def send_signup_request(
+        self, email, username, first_name, last_name, password1, password2, follow=True
+    ):
+        response = self.client.post(
+            self.signup_url,
+            data={
+                'email': email,
+                'username': username,
+                'first_name': first_name,
+                'last_name': last_name,
+                'password1': password1,
+                'password2': password2,
+            },
+            follow=follow
+        )
+        return response
+    
+    def test_user_signup(self):
+        response = self.send_signup_request(
+            self.email,
+            self.username,
+            self.first_name,
+            self.last_name,
+            self.password,
+            self.password
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(get_user(self.client), User.objects.get(username=self.username))
+        self.assertTrue(get_user(self.client).is_authenticated)
