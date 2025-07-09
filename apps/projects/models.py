c773e6648d78ad44bee199c21f67136e83b645c5
deviant <andrewsafonovpost@gmail.com>
2025-07-09 19:46:52 +0300

    Added project model

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from apps.orgs.models import Organization
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    org = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='projects')
+    description = models.TextField(max_length=500, null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('org', 'slug')
+
+    def __str__(self):
+        return self.name
