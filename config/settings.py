fb603d9c9a9a5cf85838525610347fd8fbc54f84
deviant <andrewsafonovpost@gmail.com>
2025-07-26 15:55:54 +0300

    Created logs application

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'apps.dashboard.apps.DashboardConfig',
     'apps.orgs.apps.OrgsConfig',
     'apps.projects.apps.ProjectsConfig',
+    'apps.logs.apps.LogsConfig',
 ]
 
 MIDDLEWARE = [
