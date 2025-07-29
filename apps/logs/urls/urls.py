2b423bb6c411bc17be46f0d0da3b7eb3850cdf4b
deviant <andrewsafonovpost@gmail.com>
2025-07-29 21:16:24 +0300

    Added log source deletion + confirmation

--- apps/logs/urls/urls.py
+++ apps/logs/urls/urls.py
@@ -7,5 +7,9 @@ app_name = 'logs'
 
 urlpatterns = [
     path('projects/<str:project_id>/sources/create/', views.create_log_source, name='create_log_source'),
+
+    path('sources/<str:source_id>/delete/', views.delete_log_source, name='delete_log_source'),
+    path('sources/<str:source_id>/delete/confirm/', views.delete_log_source_confirm, name='delete_log_source_confirm'),
+
     path('sources/<str:source_id>/overview/', views.log_source_overview, name='log_source_overview'),
 ]
--- apps/logs/views/views.py
+++ apps/logs/views/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from django.http import HttpResponse
+from django.urls import reverse
+
 from apps.logs.forms import LogSourceForm
 from apps.logs.models import LogSource
 
@@ -38,10 +41,53 @@ def create_log_source(request, project_id):
 
 @login_required
 def log_source_overview(request, source_id):
-    source = get_object_or_404(LogSource, id=source_id)
+    source = get_object_or_404(LogSource, pk=source_id)
 
     context = {
         'source': source,
     }
 
     return render(request, 'dashboard/logs/partials/log_source_overview_partial.html', context)
+
+
+@login_required
+def delete_log_source(request, source_id):
+    source = get_object_or_404(LogSource, pk=source_id)
+
+    context = {
+        'source': source,
+    }
+
+    return render(request, 'dashboard/logs/partials/log_source_delete_partial.html', context)
+
+@login_required
+def delete_log_source_confirm(request, source_id):
+    source = get_object_or_404(LogSource, pk=source_id)
+
+    if request.method == 'POST':
+        if 'password' in request.POST:
+            check = request.user.check_password(request.POST.get('password'))
+            if check:
+                source.delete()
+
+                messages.success(request, 'Source was permanently deleted.')
+            
+                response = HttpResponse()
+                response['HX-Redirect'] = reverse(
+                    'projects:project_details',
+                    kwargs={'org_slug': source.project.org.slug, 'project_slug': source.project.slug}
+                )
+                return response
+            else:
+                context = {
+                    'source': source,
+                    'error': 'Incorrect password.'
+                }
+
+                return render(request, 'dashboard/logs/partials/log_source_delete_confirm_partial.html', context)
+
+    context = {
+        'source': source,
+    }
+
+    return render(request, 'dashboard/logs/partials/log_source_delete_confirm_partial.html', context)
